99184036e15a42961c9d90a920078a8495e3b1de
lixuelin <lixuelin@ainirobot.com>
2020-03-31 14:57:31 +0800

    增加文件自动打包部署

---
+++ config/service.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  service: {
+    host: "59.110.231.214",
+    port: 22,
+    username: "root",
+    password: "LXL121314$%",
+    path: "/var/www/html"
+  }
+};
---
+++ utils/deploy.js
@@ -0,0 +1,11 @@
+const scpClient = require("scp2");
+const config = require("../config/service.config").service;
+// const chalk = require("chalk");
+scpClient.scp("./../dist/", config, function(err) {
+  if (err) {
+    console.log("发布失败.");
+    throw err;
+  } else {
+    console.log("Success! 成功发布到");
+  }
+});
